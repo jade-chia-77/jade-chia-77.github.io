a931103f4590b89c70e791d577ca78406ce6b890
Jade Chia <jadechia94@gmail.com>
2023-06-05 23:54:11 +1200

    added array vs object section

---
+++ blog/rubbish.js
@@ -0,0 +1,19 @@
+//ARRAY//
+let tastySnacks = ['twix', 'ice-cream', 'lollies', 'kiwi onion dip', 'chips, cheerios']
+
+//ACCESSING ARRAY ELEMENTS//
+//Accessing the 4th element aka. 'kiwi onion dip' in tastySchnacks array//
+console.log(tastySnacks[3])
+//This will return 'kiwi onion dip' in the console in dev tools//
+
+//OBJECT//
+let tastySchnacks = {
+  Schnack-1: 'Dua Lipa';
+  Schnack-2: 'Arthur Mariano';
+  Schnack-3: 'Ronan Keating';
+  Schnack-4: 'ORyan';
+}
+
+//ACCESSING OBJECT VALUES//
+console.log(tastySchnacks.Schnack-1)
+//This will return 'Dua Lipa' in the console in dev tools//
